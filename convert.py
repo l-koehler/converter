66e6eaa7dedec20a1d95c0039ae78a06659bc821
Lorenz Koehler <lorenz.koehler@posteo.de>
2023-07-25 17:06:43 +0200

    Fixed bugs in convert.py that prevented execution. (mosty stupid mistakes and typos)

--- convert.py
+++ convert.py
@@ -10,9 +10,10 @@ output_file_type = output_file_path.split('.')[-1]
 
 # Check if both input and output type are mentioned in any file in ./supported_types
 
-def check_type_support(input_file_type, output_file_type)
+def check_type_support(input_file_type, output_file_type):
 
-    entries = os.listdir(os.path.abspath(./supported_types))
+    folder_path = os.path.abspath("./supported_types")
+    entries = os.listdir(folder_path)
 
     for entry in entries:
         # Construct the full path for the entry
@@ -23,25 +24,26 @@ def check_type_support(input_file_type, output_file_type)
 
             with open(full_path, 'r') as file:
                 type_file = file.read()
-                input_types = ast.literal_eval(type_files.splitlines()[1]) # split file into lines, pick line 1 (0-based index) and convert it into a list (from a string representation of one)
-                output_types = ast.literal_eval(type_files.splitlines()[1])
+                input_types = ast.literal_eval(type_file.splitlines()[1]) # split file into lines, pick line 1 (0-based index) and convert it into a list (from a string representation of one)
+                output_types = ast.literal_eval(type_file.splitlines()[1])
 
                 if input_file_type in input_types and output_file_type in output_types:
                     # the file states conversion is possible using the converter in line 0
-                    return type_files.splitlines()[0], full_path
+                    return type_file.splitlines()[0], full_path
         else:
             pass
             # TODO: add support for recursive search later
 
-    return "Conversion not possible, no installed converter supports this."
+    return "Conversion not possible, no installed converter supports this.", ""
 
 converter_path, type_file_path = check_type_support(input_file_type, output_file_type)
-if converter_path = "Conversion not possible, no installed converter supports this.":
+if converter_path == "Conversion not possible, no installed converter supports this.":
     print("Conversion from {} to {} not possible. You can add your own converter by doing the following:".format(input_file_type, output_file_type))
     print("Step 1: Add a type file to ./supported_types."
-        "Line 1 must be a path to your converter (for relative links the origin is convert.py, NOT the type file), Line 2 a list of all supported input types and line 3 a list of all supported output types."
-        "Step 2: The converter you point to in your type file must contain a function called 'convert', with arguments like 'def convert(input_file_path, output_file_path)' where the paths are strings.'"
-        "It also must return either True or False to indicate the Success.")
+        "\nLine 1 must be a path to your converter (for relative links the origin is convert.py, NOT the type file), Line 2 a list of all supported input types and line 3 a list of all supported output types."
+        "\nStep 2: The converter you point to in your type file must contain a function called 'convert', with arguments like 'def convert(input_file_path, output_file_path)' where the paths are strings.'"
+        "\nIt also must return either True or False to indicate the Success.")
+    sys.exit(-1)
 
 # we can now assume that converter_path is a path to a python file that allows this conversion.
 converter_directory, converter_file = directory, file_name = os.path.split(converter_path)
