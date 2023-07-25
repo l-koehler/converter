ec104e4fa8d4d60a14db65423e1d52ffb0407450
Lorenz Koehler <lorenz.koehler@posteo.de>
2023-07-25 16:57:28 +0200

    Finished FFmpeg converter. Conversion should work now. TODO: Add more converters.

---
+++ converters/ffmpeg.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+def convert(input_file_path, output_file_path):
+
+    ffmpeg_command = ['ffmpeg', '-i', input_file_path, output_file_path]
+
+    try:
+        # Run FFmpeg using subprocess
+        subprocess.run(ffmpeg_command, check=True)
+        print("File conversion completed successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"File conversion failed: {e}")
+        return False
+    except FileNotFoundError:
+        print("FFmpeg executable not found. Make sure FFmpeg is installed and accessible in the system PATH.")
+        return False
+    except Exception as e:
+        print(f"An error occurred during file conversion: {e}")
+        return False
+    return True
