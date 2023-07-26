095ebde695532b41612a21b7dd1170c8552c6884
Lorenz Koehler <lorenz.koehler@posteo.de>
2023-07-26 17:23:41 +0200

    Added custom settings for .avi files

--- converters/ffmpeg.py
+++ converters/ffmpeg.py
@@ -1,18 +1,31 @@
 #!/bin/python3
 import subprocess
 
-def convert(input_file_path, output_file_path):
+def convert(input_file_path, output_file_path, force_default_codec=False):
 
-    ffmpeg_command = ['ffmpeg', '-i', input_file_path, output_file_path,]
+    ffmpeg_command = ['ffmpeg', '-i', input_file_path, output_file_path]
 
-    if output_file_path.split('.')[-1] == 'avi':
-        ffmpeg_command.append('-q:4')
+    custom_codec_used = False
+
+    if not force_default_codec:
+        if output_file_path.split('.')[-1] == 'avi':
+            custom_codec_used = True
+            ffmpeg_command.append('-c:v libx264')
+            ffmpeg_command.append('-q:v 3')
 
     try:
         # Run FFmpeg using subprocess
         subprocess.run(ffmpeg_command, check=True)
         print("File conversion completed successfully.")
     except subprocess.CalledProcessError as e:
+        if custom_codec_used == True:
+            try:
+                print("Usage of custom codec/bitrate failed, falling back to FFmpeg default settings.")
+                return convert(input_file_path, output_file_path, force_default_codec=True)
+            except Exception as e:
+                print(f"An error occurred during file conversion: {e}")
+                print("It already failed using the custom codec, the above error occured using the FFmpeg defaults.")
+                return False
         print(f"File conversion failed: {e}")
         return False
     except FileNotFoundError:
