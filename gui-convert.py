7b69ea121eb7cbfa72ceda7aa3435375aa453c57
Lorenz Koehler <lorenz.koehler@posteo.de>
2023-07-27 21:04:26 +0200

    Changed GUI program from using tkinter to PyQt5. It no longer depends on convert.py

--- gui-convert.py
+++ gui-convert.py
@@ -1,146 +1,154 @@
-import tkinter as tk
-from tkinter import ttk
-from tkinter import filedialog
-from tkinter import messagebox
-import sys, os, ast, subprocess, importlib
+#!/bin/python3
 
-def on_confirm_click():
-    input_file_path = entry_input_file.get()
-    output_file_path = entry_output_file.get()
+import sys, os, ast, importlib
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout, QHBoxLayout, QFileDialog, QComboBox, QMessageBox
 
-    input_file_type = input_file_path.split('.')[-1]
-    output_file_type = output_file_path.split('.')[-1]
+def pre_display(self):
+    # The GUI does not display yet, run code to change the contents of GUI textboxes and similar here.
+    # use command line args as in/output path if possible
+    if len(sys.argv) >= 2:
+        self.path_textbox_in.setText(sys.argv[1])
+        if len(sys.argv) >= 3:
+            self.path_textbox_out.setText(sys.argv[2])
+        # manually call set_dropdown_choices. It should be called automatically by textChanged, but that only applies to edits by the user
+        self.set_dropdown_choices(sys.argv[1])
 
-    # Check if both input and output type are mentioned in any file in ./supported_types
 
-    folder_path = os.path.dirname(os.path.abspath(__file__)) + "/supported_types"
-    folder_path = os.path.abspath(folder_path)
+    # Already declare the Variable that will store the selected converter, because it is lazily set on listing the file types for the dropdown.
+    # We need to be able to detect when it has not been touched at all to then search for the converter.
+    global converter_path
+    converter_path = ""
 
-    entries = os.listdir(folder_path)
-    converter_path, type_file_path = "no_converter_found",""
-    for entry in entries:
-        # Construct the full path for the entry
-        full_path = os.path.join(folder_path, entry)
+class FileConverter(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
 
-        # Check if the entry is a file
-        if os.path.isfile(full_path):
+    def initUI(self):
+        self.setWindowTitle('File Converter')
 
-            with open(full_path, 'r') as file:
-                type_file = file.read()
-                input_types = ast.literal_eval(type_file.splitlines()[1]) # split file into lines, pick line 1 (0-based index) and convert it into a list (from a string representation of one)
-                output_types = ast.literal_eval(type_file.splitlines()[2])
+        # Text boxes
+        self.path_textbox_in = QLineEdit(self)
+        self.path_textbox_in.textChanged.connect(self.set_dropdown_choices)
+        self.path_textbox_in.textChanged.connect(self.set_output_path)
+        self.path_textbox_out = QLineEdit(self)
 
-                if input_file_type in input_types and output_file_type in output_types:
-                    # the file states conversion is possible using the converter in line 0
-                    converter_path, type_file_path =  type_file.splitlines()[0], full_path
+        # Browse button
+        self.browse_button = QPushButton('Browse', self)
+        self.browse_button.clicked.connect(self.browse_file)
 
-    if converter_path == "no_converter_found":
-        messagebox.showerror("Error", f"Conversion from {input_file_type} to {output_file_type} not possible. You can add your own converter as described in ./README.md")
-        sys.exit(-1)
+        # Drop-down menu (ComboBox)
+        self.options_combobox = QComboBox(self)
+        self.options_combobox.addItems([])
+        self.options_combobox.currentTextChanged.connect(self.set_output_path)
 
-    # we can now assume a path to a python file that allows this conversion. import it.
-    converter_directory, converter_file = os.path.split(converter_path)
-    converter_directory = os.path.dirname(os.path.abspath(__file__)) + "/converters/"
+        # Confirm button
+        self.confirm_button = QPushButton('Confirm', self)
+        self.confirm_button.clicked.connect(self.convert)
 
-    sys.path.append(converter_directory)
-    converter = importlib.import_module(converter_file.split('.')[0])
+        # Layout setup
+        layout = QVBoxLayout()
+        layout.addWidget(QLabel('Select input file:'))
+        layout.addWidget(self.path_textbox_in)
+        layout.addWidget(self.browse_button)
+        layout.addWidget(QLabel('Select desired output location:'))
+        layout.addWidget(self.path_textbox_out)
+        layout.addWidget(QLabel('Select output format:'))
+        layout.addWidget(self.options_combobox)
+        layout.addWidget(self.confirm_button)
 
-    converter_success = converter.convert(input_file_path, output_file_path)
+        self.setLayout(layout)
+        self.show()
 
-    if converter_success == True:
-        print("Success! File converted.")
-    else:
-        messagebox.showerror("Error", f"The converter was called successfully, but returned an error.\nConverter File: {converter_path}\nType File: {type_file_path}")
+    def browse_file(self):
+        file_path, _ = QFileDialog.getOpenFileName(self, 'Select File')
+        if file_path:
+            self.path_textbox_in.setText(file_path)
 
-def browse_input_file():
-    filename = filedialog.askopenfilename()
-    entry_input_file.delete(0, tk.END)
-    entry_input_file.insert(tk.END, filename)
-
-def browse_output_file():
-    filename = filedialog.asksaveasfilename()
-    entry_output_file.delete(0, tk.END)
-    entry_output_file.insert(tk.END, filename)
-
-def update_dropdown_menu(options_list):
-    dropdown['values'] = options_list
-    dropdown.set(options_list[0])
-
-def generate_options(path="argv", no_autofill=False):
-    options = ["File Type not supported!"]
-    if path == "argv":
-        input_file_type = sys.argv[1].split('.')[-1]
-    else:
-        input_file_type = path.split('.')[-1]
-
-    folder_path = os.path.dirname(os.path.abspath(__file__)) + "/supported_types"
-    folder_path = os.path.abspath(folder_path)
-    entries = os.listdir(folder_path)
-
-    for entry in entries:
-        # Construct the full path for the entry
-        full_path = os.path.join(folder_path, entry)
-
-        # Check if the entry is a file
-        if os.path.isfile(full_path):
-
-            with open(full_path, 'r') as file:
-                type_file = file.read()
-                input_types = ast.literal_eval(type_file.splitlines()[1]) # split file into lines, pick line 1 (0-based index) and convert it into a list (from a string representation of one)
-                output_types = ast.literal_eval(type_file.splitlines()[2])
-
-                if input_file_type in input_types:
-                    if options == ["File Type not supported!"]:
-                        options = []
-                    for output_file_type in output_types:
-                        if output_file_type not in options:
-                            options.append(output_file_type)
+    def show_error(self, title="Error", text="An unexpected error occured!", info="No Information given.", is_error=True):
+        error_box = QMessageBox()
+        if is_error:
+            error_box.setIcon(QMessageBox.Critical)
         else:
-            pass # recursive search disabled
-    if no_autofill:
-        return options
-    update_dropdown_menu(options)
+            error_box.setIcon(QMessageBox.Information)
+        error_box.setWindowTitle(title)
+        error_box.setText(text)
+        error_box.setInformativeText(info)
+        error_box.setStandardButtons(QMessageBox.Ok)
+        error_box.exec_()
 
-def on_dropdown_change(*args):
-    root_path, file_extension = os.path.splitext(entry_input_file.get())
-    file_path = root_path + "." + dropdown_var.get()
-    entry_output_file.delete(0, tk.END)
-    entry_output_file.insert(tk.END, file_path)
+    def set_output_path(self):
+        # set output path to input_path with the file extension provided by the dropdown
+        input_path = self.path_textbox_in.text()
+        output_path = os.path.splitext(input_path)[0] + '.' + self.options_combobox.currentText()
+        self.path_textbox_out.setText(output_path)
 
-# Create the main tkinter window
-root = tk.Tk()
-root.title("File Converter")
+    def set_dropdown_choices(self, converter_scan=False):
+        input_path = self.path_textbox_in.text()
+        input_extension = input_path.split('.')[-1]
+        output_extension = self.path_textbox_out.text().split('.')[-1]
+        # set dropdown choices to all extensions the extension of input_path can be converted into
+        typefile_folder = os.path.dirname(os.path.abspath(__file__)) + "/supported_types"
+        entries = os.listdir(typefile_folder)
+        options = []
+        for entry in entries:
+            # Construct the full path for the entry
+            full_path = os.path.join(typefile_folder, entry)
+            # Check if the entry is a file
+            if os.path.isfile(full_path):
+                with open(full_path, 'r') as file:
+                    type_file = file.read()
+                    input_types = ast.literal_eval(type_file.splitlines()[1]) # split file into lines, pick line 1 (0-based index) and convert it into a list (from a string representation of one)
+                    output_types = ast.literal_eval(type_file.splitlines()[2])
 
-# Create input and output file labels and entry boxes
-label_input_file = tk.Label(root, text="Input File:")
-label_input_file.grid(row=0, column=0, padx=5, pady=5)
-entry_input_file = tk.Entry(root, width=50)
-entry_input_file.grid(row=0, column=1, columnspan=3, padx=5, pady=5)
-entry_input_file.insert(tk.END, sys.argv[1])  # Set sys.argv[1] as default for Input File
+                    if input_extension in input_types:
+                        print("HIT")
+                        options = list(set(output_types+options)) # merge the lists
+                        # Strictly speaking, this part does not belong here. It will store the selected converter and has nothing to do with the function.
+                        # But we can possibly avoid looping through all the directorys one extra time.
+                        if output_extension in output_types:
+                            global converter_path
+                            converter_path = type_file.splitlines()[0]
+        options.sort()
+        self.options_combobox.addItems(options)
 
-label_output_file = tk.Label(root, text="Output File:")
-label_output_file.grid(row=1, column=0, padx=5, pady=5)
-entry_output_file = tk.Entry(root, width=50)
-entry_output_file.grid(row=1, column=1, columnspan=3, padx=5, pady=5)
+    def convert(self):
+        input_path = self.path_textbox_in.text()
+        output_path = self.path_textbox_out.text()
+        input_extension = input_path.split('.')[-1]
+        output_extension = output_path.split('.')[-1]
+        typefile_folder = os.path.dirname(os.path.abspath(__file__)) + "/supported_types"
 
-# Generate a dropdown menu
-options = generate_options(no_autofill=True)
-dropdown_var = tk.StringVar(root)
-dropdown_var.set(options[0])
-dropdown_var.trace("w", on_dropdown_change)  # Attach the on_dropdown_change function to the dropdown_var
-dropdown = ttk.Combobox(root, textvariable=dropdown_var, state='readonly')
-dropdown.grid(row=2, column=0, columnspan=4, padx=5, pady=5)
-update_dropdown_menu(options)  # Initialize the dropdown menu with the options list
+        print("Path Input:", input_path)
+        print("Path Output:", output_path)
+        global converter_path
+        if converter_path == "":
+            # Global Variable is now set. If it is stlii empty, the conversion is not supported.
+            self.set_dropdown_choices(converter_scan=True)
+        if converter_path == "": # DO NOT merge with the above condition, converter_path might change!
+            self.show_error(text="Conversion failed!", info=f"Conversion from \n'{input_extension}' to '{output_extension}' not possible!\nIf you believe this is wrong, please ensure \n'{typefile_folder}'\n contains the type files.")
 
-# Create the Confirm button
-confirm_button = tk.Button(root, text="Confirm", command=on_confirm_click)
-confirm_button.grid(row=3, column=0, columnspan=4, padx=5, pady=10)
+        # we can now start using the converter
+        converter_file = os.path.split(converter_path)[-1]
+        converter_directory = os.path.dirname(os.path.abspath(__file__)) + "/converters/"
 
-# Create Browse buttons for input and output files
-browse_input_button = tk.Button(root, text="Browse", command=browse_input_file)
-browse_input_button.grid(row=0, column=4, padx=5, pady=5)
-browse_output_button = tk.Button(root, text="Browse", command=browse_output_file)
-browse_output_button.grid(row=1, column=4, padx=5, pady=5)
+        sys.path.append(os.path.abspath(converter_directory)) # add the folder the converter is in as importable, we can now import modules (converters) in there.
+        converter = importlib.import_module(converter_file.split('.')[0]) # import the converter
 
-# Start the tkinter main loop
-root.mainloop()
+        converter_success = converter.convert(input_path, output_path)
+
+        if converter_success == True:
+            print("Success! File converted.")
+            self.show_error(title="Success!", text="The file has been converted.", info="", is_error=False)
+        else:
+            self.show_error(text="Conversion failed!", info=f"The converter was called successfully,\nbut returned an error. Converter File:\n{converter_path}")
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = FileConverter()
+    # add any code you want to run on start to pre_display()
+    pre_display(window)
+    sys.exit(app.exec_())
+
