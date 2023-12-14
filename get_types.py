2797e2f796d7941f4e43b14499a7efa6dd03f860
Lorenz Koehler <lorenz.koehler@posteo.de>
2023-12-14 17:59:54 +0100

    no longer assuming tar exists, also unpacking .a/.o/.so files

--- get_types.py
+++ get_types.py
@@ -2,17 +2,22 @@
 
 import subprocess, shutil, sys, re
 
+def command_available(argv):
+    try:
+        subprocess.check_output(argv)
+        return True
+    except subprocess.CalledProcessError:
+        """
+        This only means the command returned a non-zero exit code.
+        Can be ignored, we only check for availability of the command.
+        """
+        return True
+    except FileNotFoundError:
+        return False
+
 # FFMPEG stuff
-def is_ffmpeg_installed():
-	try:
-		# Run the 'ffmpeg' command with the '-version' flag
-		# This will output the version information if FFmpeg is installed
-		subprocess.check_output(['ffmpeg', '-version'])
-		return True
-	except FileNotFoundError:
-		return False
-if is_ffmpeg_installed():
-	print("FFmpeg is installed. Generating list of supported types...")
+if command_available(['ffmpeg', '-version']):
+	print("FFmpeg is available.")
 	# FFmpeg is assumed to be functional now.
 	# Run 'ffmpeg -formats' and store the output
 	completed_process = subprocess.run(['ffmpeg', '-formats'], capture_output=True, text=True)
@@ -63,45 +68,39 @@ if is_ffmpeg_installed():
 			ffmpeg_types_content = f"ffmpeg\n{write_demux} \n{write_mux} "
 			file1.write(ffmpeg_types_content)
 else:
-	print("FFmpeg is not installed/not on the $PATH. It is required for Audio and Video conversion.")
+	print("FFmpeg is not installed/not on the $PATH. It is required for most Audio and Video files.")
 	with open("./supported_types/ffmpeg_types.txt", "w") as file:
 		file.write("ffmpeg\n \n ")
 
-"""        (always )(with ar)(with squashfs-tools)
-file from: (tar/tgz)(ar/deb )(snap/squashfs      )
-       to: (tar/tgz)(ar     )(squashfs           )
+"""        (with tar)(with ar       )(with squashfs-tools)
+file from: (tar/tgz )(ar/deb/o/a/so )(squashfs/snap      )
+       to: (tar/tgz )(ar            )(squashfs           )
 packaging software is far out-of-scope for now
 """
-def is_squashfs_installed():
-	try:
-		# Run mk- and unsquashfs commands with the '-v' flag
-		# This will output the version information if squashfs-tools is installed
-		subprocess.check_output(['unsquashfs', '-v'])
-		subprocess.check_output(['mksquashfs', '-v'])
-		return True
-	except FileNotFoundError: # only this indicates the program isnt installed.
-		return False
-	except: # all other errors can be anything else.
-		return True
-def is_ar_installed():
-    try:
-        subprocess.check_output(['ar', '-V'])
-        ar_present = True
-    except FileNotFoundError:
-        ar_present = False
+pack_str = " "
+unpack_str = " "
 
-pack_str = " tar tgz"
-unpack_str = " tar tgz"
-if is_squashfs_installed():
+# TAR
+if command_available(['tar', '--version']):
+    print("tar is available.")
+    pack_str += " tar tgz"
+    unpack_str += " tar tgz"
+else:
+    print("tar is not installed/not on the $PATH. It is required for handling .tar/.tgz files. It also is a coreutil, how do you even use your system without it?")
+# SQUASHFS
+if command_available(['mksquashfs', '-version']):
+    print("squashfs-tools is available.")
     pack_str += " squashfs"
     unpack_str += " snap sqfs squashfs"
 else:
     print("squashfs-tools is not installed/not on the $PATH. It is required for handling .snap/.squashfs files.")
-# if is_ar_installed():
-pack_str += " ar"
-unpack_str += " deb ar a"
-# else:
-#     print("ar is not installed/not on the $PATH. It is required for handling .deb/.ar/.a files.")
+# AR
+if command_available(['ar', '-V']):
+    print("ar is available.")
+    pack_str += " ar"
+    unpack_str += " ar deb a o so"
+else:
+    print("ar is not installed/not on the $PATH. It is required for handling .deb/.ar/.a files.")
 
 with open("./supported_types/compression_types.txt", "w") as file:
     file.write(f"compressed\n{unpack_str} \n{pack_str} ")
